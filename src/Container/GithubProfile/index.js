7e4bad2a5a5e7126f3fdc6d2dd5936325678659b
GarvitJ09 <garvit992001@gmail.com>
2021-12-12 23:30:16 +0530

    version 1.0.1

--- src/Container/GithubProfile/index.js
+++ src/Container/GithubProfile/index.js
@@ -1,60 +1,34 @@
-import React,{useState,useEffect} from 'react'
-import user from '../../Assets/user.png'
+import React from 'react'
 import './style.css'
-
-const GithubProfile=()=> {
-    const [name,setName]=useState('');
-    const [userName,setUsername]=useState('');
-    const [followers,setFollowers]=useState('');
-    const [following,setFollowing]=useState('');
-    const [repos,setRepos]=useState('');
-    const [avatar,setAvatar]=useState(`${user}`);
-    const [email,setEmail]=useState('');
-    const [company,setCompany]=useState('');
-    const [url,setUrl]=useState('');
-    const [userInput,setUserInput]=useState('GarvitJ09');
-    const [error,setError]=useState(null); 
-    useEffect(()=>{
-        fetch(`https://api.github.com/users/${userInput}`).then(res=>res.json())
-        .then(data=>{
-            setData(data)
+class GithubProfile extends React.Component{
+    state={
+        searchWord:'',
+        info:[]
+    }
+    componentDidMount() {
+        this.searchWord='GarvitJ09';
+        fetch(`https://api.github.com/users/${this.searchWord}`).then(res=>res.json())
+        .then(data=>{ 
+            this.setState({ info : data });  
         })
-
-    },[]);
+      }
      
- 
-    const setData=({message,name,login,followers,following,email,html_url,company,public_repos,avatar_url})=>{
-        setName(name);
-        setUsername(login);
-        setFollowers(followers);
-        setFollowing(following);
-        setRepos(public_repos);
-        setAvatar(avatar_url);
-        setEmail(email);
-        setCompany(company);
-        setUrl(html_url);
-        setError(message);
-    }
-    const handleSearch=(e)=>{
-        setUserInput(e.target.value);
-    }
-    const handleSubmit=(e)=>{ 
-        if(userInput=='')
-            alert("Enter user");
-        else if(error=="Not Found"){
-            alert("User not found");
-        }else{
-        e.preventDefault();
-        fetch(`https://api.github.com/users/${userInput}`).then(res=>res.json())
-        .then(data=>{
-            setData(data)
-        })
-    }
-    }
-
-    return (
-
-        <div className="github-profile">
+    render(){  
+        const handleSearch=(e)=>{
+            this.searchWord=e.target.value;
+        }
+        const handleSubmit=(e)=>{  
+            e.preventDefault();
+            fetch(`https://api.github.com/users/${this.searchWord}`).then(res=>res.json())
+            .then(data=>{
+                if(data["message"]=="Not Found")
+                    alert("No user found")
+                else
+                    this.setState({ info : data }); 
+            })
+        }
+        return(
+            <div className="github-profile">
             <form onSubmit={handleSubmit} style={{display:'flex',marginBottom:'10px'}}>
                 <div>
                     <input placeholder="github-user" name="github-user" onChange={handleSearch} className="search-box"></input>
@@ -64,19 +38,23 @@ const GithubProfile=()=> {
                 </div>
             </form>
             <div className="card" style={{width: '18rem'}} >
-            <img className="card-img-top" src={avatar} alt="Card image cap" />
+            <img className="card-img-top" src={this.state.info.avatar_url} alt="Card image cap" />
             <div className="card-body">
-                <h5 className="card-title">{userName}</h5>
+                <h5 className="card-title">{this.state.info.login}</h5>
                 
-                <p className="card-text"> Name: {name} </p>
-                <p className="card-text">Email: {email}</p> 
-                <p className="card-text">Company: {company}</p>
-                <p className="card-text">Followers: {followers}</p>
-                <p className="card-text">Following: {following}</p>
-                <a href={url} className="btn btn-primary" target="_blank" >Github Profile</a>
+                <p className="card-text"> Name: {this.state.info.name} </p>
+                <p className="card-text">Email: {this.state.info.email}</p> 
+                <p className="card-text">Company: {this.state.info.company}</p>
+                <p className="card-text">Followers: {this.state.info.followers}</p>
+                <p className="card-text">Following: {this.state.info.following}</p>
+                <a href={this.state.info.url} className="btn btn-primary" target="_blank" >Github Profile</a>
             </div>
             </div>
         </div>
-    )
+        );
+    }
+
 }
-export {GithubProfile};
+export {GithubProfile}
+
+
--- src/Container/HomePage/index.js
+++ src/Container/HomePage/index.js
@@ -1,5 +1,51 @@
-import React,{useState,useEffect} from 'react'
+
+
+ 
+import React from "react";
 import './style.css';
+
+ 
+
+class HomePage extends React.Component{
+    state={
+        info:[],
+        arr:["fabpot","andrew","taylorotwell","egoist","ornicar "]
+    }
+     
+
+    render(){
+        return( 
+            <div className="home-page">
+        <h3 style={{textAlign:'center'}}>Github Account Details of Some user</h3>
+        <table className="table">
+        <tr>
+            <th>Name</th>
+            <th>Followers</th>
+            <th>Following</th>
+            <th>Email</th>
+            <th>Company</th>
+        </tr>
+            { 
+                    this.state.arr.map(function(item,i){
+                        //console.log(item)
+                        fetch(`https://api.github.com/users/${item}`).then(res=>res.json())
+                        .then(data=>{   
+                        console.log(data.name) ;
+                        <Frame  name={data.name}
+                        followers={data.followers}
+                        following={data.following}
+                        email={data.email}
+                        company={data.company} /> 
+                        
+                    })
+        
+                    }) 
+              }
+              </table>
+        </div>
+        )
+    }
+}
 const Frame = ({name,followers,following,email,company}) => {  
     if(!email)
         email="none"
@@ -15,54 +61,4 @@ const Frame = ({name,followers,following,email,company}) => {
         </tr>
     );
 }
-
-const HomePage=()=> {
-    const [name,setName]=useState(''); 
-    const [followers,setFollowers]=useState('');
-    const [following,setFollowing]=useState(''); 
-    const [email,setEmail]=useState('');
-    const [company,setCompany]=useState('');
-    const [info , setInfo] = useState([]);  
-    const arr=["fabpot","andrew","taylorotwell","egoist","ornicar "];
-
-    const fetchBlogs=()=>{
-        arr.forEach((item)=>{
-            fetch(`https://api.github.com/users/${item}`).then(res=>res.json())
-            .then(data=>{
-                setInfo(arr=>[...arr , data]);
-            })
-        })
-  }
-
-
-    useEffect(() => {
-        fetchBlogs();
-      }, [])
-    console.log(info)
-    return (
-        <div className="home-page">
-        <h3 style={{textAlign:'center'}}>Github Account Details of Some user</h3>
-        <table className="table">
-        <tr>
-            <th>Name</th>
-            <th>Followers</th>
-            <th>Following</th>
-            <th>Email</th>
-            <th>Company</th>
-        </tr>
-            {
-            (info.map((data) => (
-            <Frame   
-                name={data.name}
-                followers={data.followers}
-                following={data.following}
-                email={data.email}
-                company={data.company}
-            />
-            )))
-            }
-            </table>
-        </div>
-    )
-}
-export {HomePage};
+export {HomePage}
